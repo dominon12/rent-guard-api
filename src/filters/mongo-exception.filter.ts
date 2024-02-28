883630a510528251373876b1761e6bc81f016ef1
Maksim Sobolev <sobolevmax10@gmail.com>
2024-02-28 16:25:06 +0100

    Add mongo exception filter

---
+++ src/filters/mongo-exception.filter.ts
@@ -0,0 +1,50 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { MongooseError } from 'mongoose';
+
+@Catch(MongooseError)
+export class MongoExceptionFilter implements ExceptionFilter {
+  catch(exception: MongooseError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    console.log({ name: exception.name });
+
+    let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    switch (exception.name) {
+      case 'DocumentNotFoundError':
+        statusCode = HttpStatus.NOT_FOUND;
+        break;
+
+      case 'DivergentArrayError':
+      case 'MissingSchemaError':
+      case 'ValidatorError':
+      case 'ValidationError':
+      case 'ObjectExpectedError':
+      case 'ObjectParameterError':
+      case 'OverwriteModelError':
+      case 'StrictModeError':
+      case 'VersionError':
+        statusCode = HttpStatus.BAD_REQUEST;
+        break;
+
+      case 'MongooseError':
+      case 'CastError':
+      case 'DisconnectedError':
+      case 'ParallelSaveError':
+        statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        break;
+    }
+
+    response.status(statusCode).json({
+      success: false,
+      message: exception.message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { MongoExceptionFilter } from './filters/mongo-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new MongoExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
